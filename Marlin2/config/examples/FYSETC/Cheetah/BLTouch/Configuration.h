773d13498269273d661716455eb7591b3c8738ea
Михаил Олегович <wcdnail@gmail.com>
2019-10-11 09:56:47 +0300

    Marlin update 10.10.2019

--- Marlin2/config/examples/FYSETC/Cheetah/BLTouch/Configuration.h
+++ Marlin2/config/examples/FYSETC/Cheetah/BLTouch/Configuration.h
@@ -383,6 +383,7 @@
  *    67 : 450C thermistor from SliceEngineering
  *    70 : the 100K thermistor found in the bq Hephestos 2
  *    75 : 100k Generic Silicon Heat Pad with NTC 100K MGB18-104F39050L32 thermistor
+ *    99 : 100k thermistor with a 10K pull-up resistor (found on some Wanhao i3 machines)
  *
  *       1k ohm pullup tables - This is atypical, and requires changing out the 4.7k pullup for 1k.
  *                              (but gives greater accuracy and more stable PID)
@@ -790,7 +791,7 @@
  *   http://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.02  // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.08  // (mm) Distance from real junction edge
 #endif
 
 /**
